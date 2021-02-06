3e92035c408376a711a05dafb15aa1e6d5b7b8e2
sho11hei12-1998 <sho11hei12@docomo.ne.jp>
2021-02-06 14:57:39 +0900

    modal表記のみ完成

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, Dimensions, Button, TouchableOpacity } from 'react-native';
+import { ListItem } from 'react-native-elements';
+import Modal from 'react-native-modal';
+import Icon from 'react-native-vector-icons/Feather';
+
 
 import { connect } from 'react-redux'; 
 import * as actions from '../actions'; 
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
-class DetailScreen extends React.Component {
+const modalLists = ["削除", "編集", "シェア"]
+  
 
+
+
+class DetailScreen extends React.Component {
+  constructor(props) { 
+    super(props); 
+    this.state = {
+      isModalVisible: false,
+    };
+  }
+
+  toggleModal = () => {
+    this.setState({ isModalVisible: !this.state.isModalVisible });
+  }
+
+  // 画像の描画
   renderImages() {
     // 画像が添付されていない場合の代替画像の保存場所(`uri`)
     const imageArray = [
@@ -33,6 +53,7 @@ class DetailScreen extends React.Component {
                 margin: SCREEN_WIDTH * 0.1,                
               }}
               source={image.uri}
+              key={index}
             />
           );
         })}
@@ -44,10 +65,38 @@ class DetailScreen extends React.Component {
     return (
       <View style={{ flex: 1 }}>
         <ScrollView>
-          <View style={{ alignItems: 'center', padding: 20 }}>
-            <Text style={{ fontSize: 30, padding: 5 }}>{this.props.detailReview.shopName}</Text>
-            <Text style={{ padding: 5 }}>{this.props.detailReview.date}</Text>
+          <View>
+            <View style={{ alignItems: 'center', padding: 20 }}>
+              <Text style={{ fontSize: 30, padding: 5 }}>{this.props.detailReview.shopName}</Text>
+              <Text style={{ padding: 5 }}>{this.props.detailReview.date}</Text>
+            </View>
+
+            <View style={{ flexDirection: 'row', justifyContent: 'flex-end', marginRight: 50 }}>
+              <TouchableOpacity onPress={ () => this.toggleModal() }>
+                <Icon name="more-horizontal" size={30} />
+              </TouchableOpacity>
+
+              {/* モーダル描画 */}
+              <Modal style={{justifyContent: 'flex-end'}} isVisible={this.state.isModalVisible}>
+                <View style={styles.modal}>
+                  {modalLists.map((item, idx) => {
+                    return (
+                      <ListItem key={idx} bottomDivider>
+                        <ListItem.Content style={{alignItems: 'center'}}>
+                          <TouchableOpacity><ListItem.Title>{item}</ListItem.Title></TouchableOpacity>
+                        </ListItem.Content>
+                      </ListItem>
+                    );
+                  })}
+                </View>
+                <View style={styles.cancel_modal}>
+                  <Button title="キャンセル" onPress={() => this.toggleModal()} />
+                </View>
+              </Modal>
+              
+            </View>
           </View>
+          
 
           <ScrollView 
             pagingEnabled
@@ -62,6 +111,30 @@ class DetailScreen extends React.Component {
   }
 }
 
+const styles = StyleSheet.create({ 
+  modal: {
+    justifyContent: 'center',
+    height: 135,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+
+  cancel_modal: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 50,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginTop: 10
+  },
+
+  text: {
+    color: "black",
+    fontSize: 30
+  }
+});
 
 const foodStateToProps = (state) => { 
   return {
